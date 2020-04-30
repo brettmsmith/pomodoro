62a97c9caafa830c16c01d7e148bed2a25019239
Brett Smith <brett.smith@alliant.com>
2020-04-29 17:40:29 -0700

    Added basic sound

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,32 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const zeroPad = (num: any, places: any) => String(num).padStart(places, '0');
+const audio = new Audio("/bell.mp3");
 
 function App() {
   const [time, setTime] = useState(10);
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    console.log(`Seconds: ${time}`);
     if(time > 0 && isRunning) {
         let timeout = window.setTimeout(() => {
           setTime(time - 1);
         }, 1000);
         return () => clearTimeout(timeout);
     }
+    else if(time === 0 && isRunning) {
+        audio.play();
+        setIsRunning(false);
+    }
   }, [isRunning, time]);
 
   function setTimerAndStart(time: number) {
       setTime(time);
       setIsRunning(true);
+  }
+
+  async function playSound() {
+    await audio.play();
   }
 
   return (
@@ -31,6 +39,7 @@ function App() {
         <button onClick={() => setTimerAndStart(60*25)}>Start Pomodoro</button>
         <button onClick={() => setTimerAndStart(60*5)}>Start Short Break</button>
         <button onClick={() => setTimerAndStart(60*15)}>Start Long Break</button>
+        <button onClick={() => playSound()}>Test sound</button>
     </div>
   );
 }
